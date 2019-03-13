d6b66babf5a22a207a72df36f4c0ae8d5f8cea88
Ruben Ollesch <ruben.ollesch@protonmail.com>
2019-03-13 18:43:45 +0100

    :art: Use new hot from react-hot-loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { hot } from 'react-hot-loader';
+import { hot } from 'react-hot-loader/root';
 
 const App = () => (<main>Hello World</main>);
 
-export default hot(module)(App);
+export default hot(App);
